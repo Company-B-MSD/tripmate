050d3e6b02b994a5c213c4b0d9d4fdccc4aad81b
malshiwijerathna <malshiwijerathna60@gmail.com>
2025-07-10 14:14:06 +0530

    Add CORS config file

---
+++ backend/OAuth2/SpringOAuth2/src/main/java/com/tripmate/SpringOAuth2/config/CorsConfig.java
@@ -0,0 +1,26 @@
+package com.tripmate.SpringOAuth2.config;
+
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("https://tripmate-ecru.vercel.app"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
+    }
+}
